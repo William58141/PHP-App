e0d40d1e1876a9fb2894d03b483b4d58993e7b97
William Fosmark Haugland <31375260+William58141@users.noreply.github.com>
2022-06-05 20:46:34 +0200

    Oppdaterte db-tilkobling

--- db-tilkobling.php
+++ db-tilkobling.php
@@ -2,8 +2,8 @@
 
 $host="localhost";
 
-$user="233584";
-$password="Fo+uF89h";
+$user="root";
+$password="root";
 $database="233584";
 
 $db=mysqli_connect($host,$user,$password,$database) or die ("ikke kontakt med database-server");
